59f8ea3c93fa197b2e1a3d2261a2b39f029ad462
JonathanMooMijares <l22390450@chetumal.tecnm.mx>
2023-03-21 15:02:59 -0500

    !1.

---
+++ src/JuegoDado.java
@@ -0,0 +1,5 @@
+public class JuegoDado {
+
+}
+//hacer un ciclo, iniciar primero con iniciar juego, luego elegirPrimerlanzador de manera aleatoria
+//en elegirprimerlanzador hacer un ciclo, de ahí usar
