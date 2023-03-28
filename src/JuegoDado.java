523cadbf13793c2e4fefffcf595638bb48fee619
JonathanMooMijares <l22390450@chetumal.tecnm.mx>
2023-03-28 15:02:00 -0500

    !1.2.3.4

--- src/JuegoDado.java
+++ src/JuegoDado.java
@@ -6,7 +6,15 @@ public class JuegoDado {
     int marcadorJugador2;
     Dado dado1;
     Dado dado2;
+    Jugador vencedor;
     private boolean bandJugador; //true = Jugador 1, false = Jugador2
+
+    public JuegoDado(String nombreJugador1, String nombreJugador2){
+        this.jugador1 = new Jugador();
+        this.jugador1.nombre = nombreJugador1;
+        this.jugador2 = new Jugador();
+        this.jugador2.nombre = nombreJugador2;
+    }
 
     public void elegirPrimerLanzador(){
         if ((byte) (Math.random() * (3 - 1)) == 1){
@@ -19,21 +27,27 @@ public class JuegoDado {
         }
     }
 
-    public void iniciarJugada() {
+    public void iniciarJugada(){
         Jugada jugadaActual = new Jugada();
 
         if (this.bandJugador) {
-            jugadaActual.iniciarJugada(this.jugador1, this.jugador2, this.dado1, this.dado2);
-                this.dado1, this.dado2);
+            jugadaActual.iniciarJugada(this.jugador1, this.jugador2,
+                    this.dado1, this.dado2);
         }
-        else{
-            jugadaActual.iniciarJugada(this.jugador2, this.jugador1)
+        else {
+            jugadaActual.iniciarJugada(this.jugador2, this.jugador1,
+                    this.dado1, this.dado2);
         }
+
+        this.marcadorJugador1 = this.marcadorJugador1 + this.jugador1.puntoGanado;
+        this.marcadorJugador2 = this.marcadorJugador2 + this.jugador2.puntoGanado;
     }
     public void iniciarJuego() {
         //Crear jugadores, dados, inicializar variables
-        this.jugador1 = new Jugador();
-        this.jugador2 = new Jugador();
+        //this.jugador1 = new Jugador();
+        //this.jugador1.nombre = "jugador 1";
+        //this.jugador2 = new Jugador();
+        //this.jugador2.nombre = "jugador 2";
 
         this.dado1 = new Dado();
         this.dado2 = new Dado();
@@ -47,13 +61,38 @@ public class JuegoDado {
             iniciarJugada();
 
             this.cantidadJugada++;
-        } while ((this.marcadorJugador1 == 5) || (this.marcadorJugador2 ==5));
+/*
+            System.out.println("Num. jugada: " + this.cantidadJugada
+                    + " puntaje jugador 1= " + this.marcadorJugador1
+                    + " puntaje jugador 2= " + this.marcadorJugador2);
 
-        elegirPrimerLanzador();
+ */
 
 
+        } while ((this.marcadorJugador1 != 5) && (this.marcadorJugador2 !=5));
 
+
+        //Determinad ganador
+        this.vencedor= determinarVencedor();
     }
+
+    public Jugador determinarVencedor(){
+        //caso empate
+        if ((this.marcadorJugador1 == 5) && (this.marcadorJugador2 ==5))
+            return null;
+
+        //ganó el jugador1
+        if (this.marcadorJugador1 == 5) {
+            return jugador1;
+        }
+            else { //ganó el jugador 2
+                if (this.marcadorJugador2 == 5) {
+                    return jugador2;
+                }
+        }
+        return null;
+    }
+
 }
 
 //hacer un ciclo, iniciar primero con iniciar juego, luego elegirPrimerlanzador de manera aleatoria
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JuegoDado juego = new JuegoDado("Pedro", "José");
+        juego.iniciarJuego();
+        //juego.jugador1.nombre = "Pedro";
+        //juego.jugador1.nombre = "José";
+/*
+        while (juego.vencedor != null){
+
+        }
+ */
+        juego.iniciarJuego();
+        if (juego.vencedor == null)
+            System.out.println ("¡¡Empate!! No hay un vencedor");
+        else
+            System.out.println  ("El vencedor es: " +juego.vencedor.nombre);
     }
 }
